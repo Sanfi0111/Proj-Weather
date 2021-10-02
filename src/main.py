cbf8202542a85a448f4d206118e13f53207355a8
Santiago Díaz <santi@ciencias.unam.mx>
2021-10-02 12:05:05 -0500

    Ahora lee el csv almacena en arreglos

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import csv
 
 file = open('src\dataset1.csv')
-print('llegue')
 csvreader = csv.reader(file)
 header = next(csvreader)
 print(header)
@@ -9,5 +8,4 @@ rows = []
 for row in csvreader:
     rows.append(row)
 print(rows)
-print('llegue')
 file.close()
