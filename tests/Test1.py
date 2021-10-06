79977c44008894b59834be2a10d849af11d9d2f0
Sanfi0111 <santi@ciencias.unam.mx>
2021-10-06 11:37:28 -0500

    Añadí los test, falta implementar unittest

--- tests/Test1.py
+++ tests/Test1.py
@@ -1,7 +1,12 @@
+import unittest
 
+class Test1(unittest.TestCase):
 
-def findReadDataSetElement(lat, lon):
-    return
+    def findReadDataSetElement(lat, lon):
+        # assertTrue asegura que un valor es real
+        # assertFalse asegura que un valor es falso
+        return
 
-def testLatLongNotExists():
-    return
+    def testLatLongNotExists():
+        return
+    
