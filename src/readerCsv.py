c110ae4aa465685e7654f6c447755f6c5d894af6
Sanfi0111 <santi@ciencias.unam.mx>
2021-10-07 12:23:43 -0500

    Ahora ya corre todos los requests con sus mensajes en forma correspondientes

--- src/readerCsv.py
+++ src/readerCsv.py
@@ -1,18 +1,19 @@
 import csv
 # Este ya tiene la informacion de las latitudes y longitudes de las cuidades de origen y destino, sin repeticiones
+# i)Hacer un test por si no se encontro alguna ciudad que está en el diccionario proveniente de readDataSet()+
+#ii) longitud no existe o latitud
 def readDataNoRep():
     file = open('src\dataset1.csv')
     csvreader = csv.DictReader(file)
     dictCoorOrigin = {}
-    dictCoorDest = {}
     for row in csvreader:
         dictCoorOrigin[row['origin']] = (row['origin_latitude'],row['origin_longitude'])
-        dictCoorDest[row['destination']] = (row['destination_latitude'],row['destination_longitude'])
+        dictCoorOrigin[row['destination']] = (row['destination_latitude'],row['destination_longitude'])
     print(dictCoorOrigin)
-    print(dictCoorDest)
-    return (dictCoorOrigin, dictCoorDest)
-    file.close()
+    print(str(len(dictCoorOrigin)))
+    return (dictCoorOrigin)
 
+    file.close()
 # Este lee el csv completo y nos regresa el diccionario donde se guardó toda la información
 def readDataSet():
     file = open('src\dataset1.csv')
@@ -20,10 +21,9 @@ def readDataSet():
     rows = {}
     i = 0
     for row in csvreader:
-        row[i] = row
+        rows[i] = row
         i += 1
     return rows
-readDataSet()
 
 
 
--- src/requestKey.py
+++ src/requestKey.py
@@ -1,14 +1,16 @@
 import requests
+import time
+from readerCsv import readDataSet
 #Esta es la llave para poder acceder a la API
 key ='a00bd54d69b3cdd04b682f93586e3512'
 #URL para revisar si la llave sirve bien
 url = 'http://api.openweathermap.org/data/2.5/weather?'
 resp = requests.get(url).json()
-lon = '-99.566'
-lat = '19.337'
-resp1= 'http://api.openweathermap.org/data/2.5/weather?lat=19.337&lon=-99.566&appid='+key
-resp3=requests.get(resp1).json()
-print(resp3)
+# lon = '-99.566'
+#lat = '19.337'
+#resp1= 'http://api.openweathermap.org/data/2.5/weather?lat=19.337&lon=-99.566&appid='+key
+#resp3=requests.get(resp1).json()
+#print(resp3)
 
 def printForm(con, weath, sens, tempmax, tempmin, hume):
     cadenaCiu = con+": \n"
@@ -17,10 +19,47 @@ def printForm(con, weath, sens, tempmax, tempmin, hume):
     cadenaHume = "Humedad: "+hume
     cadenaCompleta = cadenaCiu + cadenaCli + cadenaSens + cadenaHume 
     
-# Con readDataSet() voy accediendo a 
-def request(latlonOri, latlonDes, dicDataSet):
-    completCoor = latlon.split(',')
-    urlComp = url+'lat='+latlon[0]+"&lon="+latlon[0]+'&appid='+key+'&lang=es&units=metric'
-    request = requests.get(urlComp).json()
+""" Request a city weather by coordenates
+    dicDataSet: the complete dictionary that contains all the information from dataset1.csv
+    return: The weather of the requested city """
+def request(dicDataSet):
+    compDatOrigin = {}
+    compDatDestin = {}
+    cadenasOri = []
+    cadenasDest = []
+    i = 0
+    for row in dicDataSet:
+        urlCompIda = url+'lat=' +dicDataSet[i]['origin_latitude']+"&lon=" +dicDataSet[i]['origin_longitude']+ '&appid=' + key + '&lang=es&units=metric'
+        urlCompDest = url+'lat=' +dicDataSet[i]['destination_latitude']+"&lon=" +dicDataSet[i]['destination_longitude']+ '&appid=' + key + '&lang=es&units=metric'
+        try:
+            compDatOrigin = requests.get(urlCompIda).json()
+            cadenasOri = inList(compDatOrigin)
+            print(cadenasOri)
+            time.sleep(1)
+            compDatDestin = requests.get(urlCompDest).json()
+            cadenasDest = inList(compDatDestin)
+        except(KeyError):
+            print("No se pudo encontrar una ciudad con las coordenadas ingresadas")
+        i += 1
+    print(cadenasOri)
+
+""" Add elements from a request into a List 
+    dicRequests: A dictionary containing the requests"""
+def inList(dicRequests):
+    cadenas = []
+    cadenaciuOr = (dicRequests['name'])
+    cadenas.append(cadenaciuOr)
+    cadenaMinOr = (dicRequests['main']['temp_min'])
+    cadenas.append(cadenaMinOr)
+    cadenaMaxOr = (dicRequests['main']['temp_max'])
+    cadenas.append(cadenaMaxOr)
+    cadeaHumOr = (dicRequests['main']['humidity'])
+    cadenas.append(cadeaHumOr)
+    cadenaSensOr = (dicRequests['main']['feels_like'])
+    cadenas.append(cadenaSensOr)
+    return cadenas 
+
+
+request(readDataSet())
 
 
