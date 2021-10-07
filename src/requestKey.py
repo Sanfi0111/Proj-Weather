3d9e20dcd4a278ee5bfede6a339c410e0d77140b
Sanfi0111 <santi@ciencias.unam.mx>
2021-10-07 14:02:07 -0500

    Request ahora implementa printForm

--- src/requestKey.py
+++ src/requestKey.py
@@ -11,13 +11,15 @@ resp = requests.get(url).json()
 #resp1= 'http://api.openweathermap.org/data/2.5/weather?lat=19.337&lon=-99.566&appid='+key
 #resp3=requests.get(resp1).json()
 #print(resp3)
-
-def printForm(con, weath, sens, tempmax, tempmin, hume):
-    cadenaCiu = con+": \n"
-    cadenaCli = "Clima: "+ weath+" Temperatura máxima:"+tempmax+", Temperatura mínima :"+tempmin
-    cadenaSens = "Sensación térmica:"+sens
-    cadenaHume = "Humedad: "+hume
+""" Prints a weather summary
+    Weather: a list containing the weather of a city """
+def printForm(weather):
+    cadenaCiu = weather[0]+": \n"
+    cadenaCli = "Clima: "+str(weather[1])+ " Temperatura máxima: "+str(weather[2])+ ", Temperatura mínima : "+str(weather[3])+"\n"
+    cadenaSens = "Sensación térmica:"+str(weather[4])+"\n"
+    cadenaHume = "Humedad: "+str(weather[4])+"\n"
     cadenaCompleta = cadenaCiu + cadenaCli + cadenaSens + cadenaHume 
+    print(cadenaCompleta)
     
 """ Request a city weather by coordenates
     dicDataSet: the complete dictionary that contains all the information from dataset1.csv
@@ -33,15 +35,18 @@ def request(dicDataSet):
         urlCompDest = url+'lat=' +dicDataSet[i]['destination_latitude']+"&lon=" +dicDataSet[i]['destination_longitude']+ '&appid=' + key + '&lang=es&units=metric'
         try:
             compDatOrigin = requests.get(urlCompIda).json()
+            print("Origen:")
             cadenasOri = inList(compDatOrigin)
-            print(cadenasOri)
-            time.sleep(1)
+            printForm(cadenasOri)
             compDatDestin = requests.get(urlCompDest).json()
+            print("Destino:")
             cadenasDest = inList(compDatDestin)
+            printForm(cadenasDest)
+            time.sleep(1)
+
         except(KeyError):
             print("No se pudo encontrar una ciudad con las coordenadas ingresadas")
         i += 1
-    print(cadenasOri)
 
 """ Add elements from a request into a List 
     dicRequests: A dictionary containing the requests"""
@@ -49,6 +54,8 @@ def inList(dicRequests):
     cadenas = []
     cadenaciuOr = (dicRequests['name'])
     cadenas.append(cadenaciuOr)
+    cadenaTemp = (dicRequests['main']['temp'])
+    cadenas.append(cadenaTemp)
     cadenaMinOr = (dicRequests['main']['temp_min'])
     cadenas.append(cadenaMinOr)
     cadenaMaxOr = (dicRequests['main']['temp_max'])
