6a57499729adbcbd128f01597c1a8e985a26711d
Sanfi0111 <santi@ciencias.unam.mx>
2021-10-09 16:19:42 -0500

    Empiezan tests

--- tests/Test1.py
+++ tests/Test1.py
@@ -1,11 +1,12 @@
 import unittest
-
+import readerCsv
+import requestKey
 class Test1(unittest.TestCase):
 
     def findReadDataSetElement(lat, lon):
         # assertTrue asegura que un valor es real
         # assertFalse asegura que un valor es falso
-        return
+        result  = 
 
     def testLatLongNotExists():
         return
